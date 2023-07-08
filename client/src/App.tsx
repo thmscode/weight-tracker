b5cb9ebcdfd3144a4e8d1990bc5eb3edb7d317db
thmscode <102998926+thmscode@users.noreply.github.com>
2023-07-07 21:26:08 -0400

    Set up basic routing
    Added a router and configured it for the SPA

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,26 @@
-function App() {
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './layouts/Root';
+import ErrorPage from './pages/ErrorPage';
+import Homepage from './pages/Homepage';
+import BMI from './pages/BMI';
+import Workouts from './pages/Workouts';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <Homepage /> },
+      { path: "/bmi", element: <BMI /> },
+      { path: "/workouts", element: <Workouts /> },
+    ]
+  },
+]);
+
+const App = () => {
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
