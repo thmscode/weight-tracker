e98864b285bd1895d7dcfeaab0701c9e2cb49ca4
thmscode <102998926+thmscode@users.noreply.github.com>
2023-07-20 18:00:55 -0400

    Create constants.ts
    Created a file in /utils that houses constant values used in the project

--- client/src/pages/BMI/BMIChart.tsx
+++ client/src/pages/BMI/BMIChart.tsx
@@ -4,14 +4,7 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-
-const HEADERS = ['Classification', 'BMI'];
-const CATEGORIES = [
-  { class: 'Underweight', value: '<18.5' },
-  { class: 'Normal Weight', value: '18.5 - 24.9' },
-  { class: 'Overweight', value: '25.0 - 29.9' },
-  { class: 'Obesity', value: '≥30.0' }
-];
+import { HEADERS, CATEGORIES } from '../../utils/constants';
 
 const BMIChart = () => {
   return (
---
+++ client/src/utils/constants.ts
@@ -0,0 +1,8 @@
+// BMI Page - BMIChart.tsx
+export const HEADERS = ['Classification', 'BMI'];
+export const CATEGORIES = [
+  { class: 'Underweight', value: '<18.5' },
+  { class: 'Normal Weight', value: '18.5 - 24.9' },
+  { class: 'Overweight', value: '25.0 - 29.9' },
+  { class: 'Obesity', value: '≥30.0' }
+];
