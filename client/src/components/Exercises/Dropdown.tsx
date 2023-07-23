57966054a0ab133ec3150df0c83c8b71c6b258be
thmscode <102998926+thmscode@users.noreply.github.com>
2023-07-22 23:15:16 -0400

    Create Dropdown.tsx

---
+++ client/src/components/Exercises/Dropdown.tsx
@@ -0,0 +1,34 @@
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import { Field } from "formik";
+
+type Props = {
+  id: string;
+  label: string;
+  options: string[];
+};
+
+const Dropdown: React.FC<Props> = ({ id, label, options }) => {
+  return (
+    <FormControl sx={{ flexGrow: 1 }}>
+      <InputLabel id={id}>{label}</InputLabel>
+      <Field
+        as={Select}
+        id={id}
+        name={id}
+        label={label}
+      >
+        <MenuItem key={`${id}-0`} value=''>None</MenuItem>
+        {options.map((option, index) =>
+          <MenuItem key={`${id}-${index}`} value={option}>{option}</MenuItem>
+        )}
+      </Field>
+    </FormControl>
+  );
+}
+
+export default Dropdown;
