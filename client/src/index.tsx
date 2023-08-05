b8d46e50ae8b87c547c90585a0349419483f8ceb
thmscode <102998926+thmscode@users.noreply.github.com>
2023-08-05 02:29:27 -0400

    Update index.tsx
    Configured Auth0 and added it to the application using the Auth0 React SDK

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN ?? '';
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID ?? '';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
