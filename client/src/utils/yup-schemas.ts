c0b9c0362b34836f21d1e0712a2c63d254b475d3
thmscode <102998926+thmscode@users.noreply.github.com>
2023-09-05 20:54:23 -0400

    Update yup-schemas.ts

--- client/src/utils/yup-schemas.ts
+++ client/src/utils/yup-schemas.ts
@@ -3,8 +3,21 @@ import * as Yup from 'yup';
 export const BMI_FORM_VALIDATION = Yup.object().shape({
   height: Yup.number()
     .min(1, 'Please enter a valid height')
-    .required('Height is required'),
+    .required('Height is required')
+    .typeError('Height must be a number'),
   weight: Yup.number()
     .min(1, 'Please enter a valid weight')
     .required('Weight is required')
+    .typeError('Weight must be a number')
+});
+
+export const USER_DATA_FORM_VALIDATION = Yup.object().shape({
+  height: Yup.number()
+    .min(1, 'Please enter a valid height')
+    .max(120, 'Please enter a valid height')
+    .typeError('Height must be a number'),
+  weight: Yup.number()
+    .min(1, 'Please enter a valid weight')
+    .max(2000, 'Please enter a valid weight')
+    .typeError('Height must be a number')
 });
