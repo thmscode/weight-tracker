1b5adc2398313b874c8bee8c7df6925eb13eef3c
thmscode <102998926+thmscode@users.noreply.github.com>
2023-09-07 20:50:10 -0400

    Add new route & controller fn

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 
+const calculateBMI = (h: number, w: number): number => {
+  const BMI = (w / Math.pow(h, 2)) * 703;
+  return Math.round(BMI * 10) / 10;
+};
+
 export const getUserData = async (req: Request, res: Response) => {
   const { email, id } = req.query;
 
@@ -11,5 +16,19 @@ export const getUserData = async (req: Request, res: Response) => {
   } catch (e) {
     console.log(e);
     res.status(500).json({ error: 'Internal server error.', data: null });
+  }
+};
+
+export const updateUserData = async (req: Request, res: Response) => {
+  const { email, id } = req.query;
+  const { height, weight } = req.body;
+  const bmi = calculateBMI(height, weight);
+
+  try {
+    await User.findOneAndUpdate({ email, id }, { height, weight, bmi });
+    res.status(200).json({ error: null, msg: 'User updated.' });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: 'Internal server error.', msg: 'Internal server error.' });
   }
 };
--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getUserData } from "../controllers/user";
+import {
+  getUserData,
+  updateUserData,
+} from "../controllers/user";
 
 const router = Router();
 
 router.get('/', getUserData);
+router.post('/updateUserData', updateUserData);
 
 export default router;
