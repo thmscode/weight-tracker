5d6d274f9c086ac2dbf3cf158b3b3b8d75527624
thmscode <102998926+thmscode@users.noreply.github.com>
2023-09-20 02:41:27 -0400

    Create yup-schemas.ts
    Defined schemas that will be used to validate the bodies of incoming requests

---
+++ server/yup-schemas.ts
@@ -0,0 +1,32 @@
+import * as Yup from 'yup';
+
+export const updateUserSchema = Yup.object({
+  height: Yup.number()
+    .min(1)
+    .max(120)
+    .required(),
+  weight: Yup.number()
+    .min(1)
+    .max(2000)
+    .required()
+});
+
+export const deleteEntrySchema = Yup.object({
+  dateArray: Yup.array()
+    .of(Yup.number())
+    .min(3)
+    .max(3)
+    .required()
+});
+
+export const entrySchema = Yup.object({
+  weight: Yup.number()
+    .min(1)
+    .max(2000)
+    .required(),
+  dateArray: Yup.array()
+    .of(Yup.number())
+    .min(3)
+    .max(3)
+    .required()
+});
