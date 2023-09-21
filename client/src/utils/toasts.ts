399726785edac78ca60667a53a108110485f5f92
thmscode <102998926+thmscode@users.noreply.github.com>
2023-09-21 02:17:27 -0400

    Create toasts.ts
    Followed the React-Toastify documentation and created reusable success & error toast pop-ups to display feedback from the server to the users

---
+++ client/src/utils/toasts.ts
@@ -0,0 +1,25 @@
+import { toast } from 'react-toastify';
+
+export const renderSuccessToast = (msg: string) =>
+  toast.success(msg, {
+    position: "bottom-center",
+    autoClose: 1000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+
+export const renderErrorToast = (msg: string) =>
+  toast.error(msg, {
+    position: "bottom-center",
+    autoClose: 1000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
